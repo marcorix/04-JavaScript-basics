2090c37bec594b4376e8c55251929500c8813bd0
Marco Ricci <mr.ricci79@gmail.com>
2022-11-07 12:20:35 +0000

    list generstor completed

--- 04-Unordered-list/lib/list_generator.js
+++ 04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,14 @@
-const listItem = (content) => {
-  // TODO: return the proper <li> HTML tag with its content (as a string)
-};
+const listItem = content => `<li class="list-group-item">${content}</li>`;
+
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  let html = "";
+  items.forEach((element) => {
+    html += listItem(element);
+  });
+  return `<ul class="list-group">${html}</ul>`;
 };
 
 module.exports = { listItem, unorderedList }; // Do not remove.
+console.log(unorderedList(["milk", "butter", "bread"]));
